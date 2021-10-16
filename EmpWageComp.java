0819f28e0d94167ab640343235a1726453a7c2f3
Artimirche6 <artimirche6@gmail.com>
2021-10-16 18:35:38 +0530

    [arti]: Daily Employee Wage

--- EmpWageComp.java
+++ EmpWageComp.java
@@ -2,21 +2,29 @@ public class EmpWageComp
 {
 	public static void main(String[] args)
 	{
+		//Contants
 		int IS_FULL_TIME = 1;
-
-		double empCheck=Math.floor(Math.random() * 10) % 2;
-
-		if ( empCheck == IS_FULL_TIME )
+		int EMP_RATE_PER_HOUR = 20;
+		
+		//Variables
+		int empHrs = 0;
+		int empWage = 0;
+		
+		//Computation
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		
+		if (empCheck == IS_FULL_TIME)
 		{
-			System.out.println("Employee is Present");
+			empHrs = 8;
 		}
 		else
 		{
-			System.out.println("Employee is Absent");
+			empHrs = 0;
 		}
+		
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage : "+empWage);
+		
 
 	}
-
-}
-
 }
