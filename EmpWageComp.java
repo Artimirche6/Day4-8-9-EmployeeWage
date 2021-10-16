bf24c512536e6a0826e6c3e71b1c7d073df0a284
Artimirche6 <artimirche6@gmail.com>
2021-10-16 18:45:41 +0530

    [arti]: Employee Wage using if else statement

--- EmpWageComp.java
+++ EmpWageComp.java
@@ -1,30 +1,30 @@
 public class EmpWageComp
 {
-	//Contants
-		public static final int IS_FULL_TIME = 1;
-		public static final int IS_PART_TIME = 2;
-		public static final int EMP_RATE_PER_HOUR = 20;
-		
 	public static void main(String[] args)
-	{	
+	{
+		//Contants
+		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
+		int EMP_RATE_PER_HOUR = 20;
+		
 		//Variables
 		int empHrs = 0;
 		int empWage = 0;
 		
 		//Computation
-		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
 		
-		switch(empCheck)
+		if (empCheck == IS_FULL_TIME)
 		{
-			case IS_FULL_TIME:
-				empHrs = 8;
-			break;
-			case IS_PART_TIME:
-				empHrs = 4;
-			break;
-			default:
-				empHrs = 0;
-			break;
+			empHrs = 8;
+		}
+		else if (empCheck == IS_PART_TIME)
+		{
+			empHrs = 4;
+		}
+		else
+		{
+			empHrs = 0;
 		}
 		
 		empWage = empHrs * EMP_RATE_PER_HOUR;
