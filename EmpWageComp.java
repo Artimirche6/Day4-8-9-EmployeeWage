7ca2eb11562e7f49d513db02ec316e9dab230e96
Artimirche6 <artimirche6@gmail.com>
2021-10-16 18:53:16 +0530

    [arti]: Calculate Wages till a condition of total working hours or days is reached for a month

--- EmpWageComp.java
+++ EmpWageComp.java
@@ -1,22 +1,24 @@
 public class EmpWageComp
 {
-	
 	//Contants
 		public static final int IS_FULL_TIME = 1;
 		public static final int IS_PART_TIME = 2;
 		public static final int EMP_RATE_PER_HOUR = 20;
 		public static final int NUM_OF_WORKING_DAYS = 2;
+		public static final int MAX_HRS_IN_MONTH = 10;
 		
 	public static void main(String[] args)
 	{	
 		//Variables
-		int empHrs = 0;
-		int empWage = 0;
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+		//int empWage = 0;
 		int totalEmpWage = 0;
 		
 		//Computation
-		for ( int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+		while ( totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
 		{
+			totalWorkingDays++;
+			
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 		
 			switch(empCheck)
@@ -32,13 +34,11 @@ public class EmpWageComp
 				break;
 			}
 		
-			empWage = empHrs * EMP_RATE_PER_HOUR;
-			totalEmpWage += empWage;
-			System.out.println("Emp Wage : "+empWage);
+			totalEmpHrs += empHrs;
+			System.out.println("Day# : "+totalWorkingDays+ " Emp Hrs : " +empHrs);
 		}
-		
+		totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Total Emp Wage : "+totalEmpWage);
 
 	}
 }
-  
