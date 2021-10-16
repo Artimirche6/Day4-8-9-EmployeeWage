73ce2f49da8b0421f73a04aa5f42a0283b8576b7
Artimirche6 <artimirche6@gmail.com>
2021-10-16 18:31:47 +0530

    [arti]: UC1 branch data load master

--- EmpWageComp.java
+++ EmpWageComp.java
@@ -1,9 +1,19 @@
 public class EmpWageComp
 {
-	public static void main(String args[])
+	public static void main(String[] args)
 	{
+		int IS_FULL_TIME = 1;
 
-			System.out.println("Welcome to Employee Wage Computation in master");
+		double empCheck=Math.floor(Math.random() * 10) % 2;
+
+		if ( empCheck == IS_FULL_TIME )
+		{
+			System.out.println("Employee is Present");
+		}
+		else
+		{
+			System.out.println("Employee is Absent");
+		}
+
 	}
-
 }
