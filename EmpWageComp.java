ecd4e654a38e14e80f7b00742743fb4019d7df4c
Artimirche6 <artimirche6@gmail.com>
2021-10-19 17:02:07 +0530

    [arti] :Refactor the code using Class Method to compute Employee Wage

--- EmpWageComp.java
+++ EmpWageComp.java
@@ -1,14 +1,14 @@
 public class EmpWageComp
 {
 	//Contants
-		public static final int IS_FULL_TIME = 1;
-		public static final int IS_PART_TIME = 2;
-		public static final int EMP_RATE_PER_HOUR = 20;
-		public static final int NUM_OF_WORKING_DAYS = 2;
-		public static final int MAX_HRS_IN_MONTH = 10;
-		
-	public static void main(String[] args)
-	{	
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
+	
+	public static int computEmpWage()
+	{
 		//Variables
 		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 		//int empWage = 0;
@@ -37,8 +37,20 @@ public class EmpWageComp
 			totalEmpHrs += empHrs;
 			System.out.println("Day# : "+totalWorkingDays+ " Emp Hrs : " +empHrs);
 		}
+		
+		System.out.println("Total Emp Hrs : "+totalEmpHrs);
 		totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Total Emp Wage : "+totalEmpWage);
+		return totalEmpWage;
+	
+	}
+	
+	
+	public static void main(String[] args)
+	{	
+
+		computEmpWage();
 
 	}
+	
 }
